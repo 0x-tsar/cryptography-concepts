9fde46ab49d3c1b01941f6c0688f15b2e92402dc
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-01 15:25:52 -0300

    check

--- crypto.js
+++ crypto.js
@@ -4,5 +4,15 @@ const { createHash } = require("crypto");
 function hash(input) {
   //returns  a hash value called `digest` which returns  a
   //256-bit
-  return createHash("sha256"); //stands for secure hash algorithm
+  //digest == output
+  //SHA stands for secure hash algorithm
+  return createHash("sha256").update(input).digest("hex");
 }
+
+let password = "hi-mom!";
+const hash1 = hash(password);
+
+let password2 = "hi-momw!";
+const hash2 = hash(password2);
+
+console.log(hash1 === hash2);
